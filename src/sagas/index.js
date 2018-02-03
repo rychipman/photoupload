ef573c7f106c777099560b294f5e18d1fbe3fd25
Ryan Chipman <ryan@ryanchipman.com>
2018-02-02 20:52:51 -0500

    Authenticate api calls with jwt

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -15,49 +15,78 @@ import {
 
 const api = {}
 api.upload = (file) => {
+    const token = localStorage.getItem('jwtToken')
+    console.log('sending api request with token: ' + token)
     const formData = new FormData()
     formData.append('file', file)
     return fetch('http://localhost:8080/upload', {
         method: 'POST',
-        headers:{ },
+        headers: new Headers({
+            'Authorization': `Bearer ${token}`,
+        }),
         body: formData,
-    }).then(res => res.json())
+    }).then(res => {
+        if (res.ok) {
+            return res.json()
+        }
+        throw new Error('response was not ok')
+    }).then(json => ({
+        ok: true,
+        hash: json.hash,
+    })).catch(e => (
+        Promise.resolve({
+            ok: false,
+            message: e.message,
+        })
+    ))
 }
 
 function* uploadFile(action) {
     yield put(fileUploading(action.id))
-
-    try {
-        const res = yield call(api.upload, action.file)
-        if (res.success) {
-            console.log(res)
-            yield put(fileUploaded(action.id, res.data.hash))
-        } else {
-            console.log(res)
-            yield put(fileUploadFailed(action.id))
-            yield put(createNotification('upload failed'))
-        }
-    } catch(e) {
-        console.log(e)
+    const res = yield call(api.upload, action.file)
+    console.log(res)
+    if (res.ok) {
+        yield put(fileUploaded(action.id, res.hash))
+    } else {
         yield put(fileUploadFailed(action.id))
-        yield put(createNotification('upload failed with uncaught err'))
+        yield put(createNotification('upload failed'))
     }
-
 }
 
 api.getToken = (email, password) => {
     return fetch('http://localhost:8080/token', {
-        method: 'GET',
-    }).then(res => res.json())
+        method: 'POST',
+        headers: new Headers({
+            'Content-Type': 'application/json',
+        }),
+        body: JSON.stringify({
+            email,
+            password,
+        }),
+    }).then(res => {
+        if (res.ok) {
+            return res.json()
+        }
+        throw new Error('response was not ok')
+    }).then(json => ({
+        ok: true,
+        token: json.token,
+    })).catch(e => (
+        Promise.resolve({
+            ok: false,
+            message: e.message,
+        })
+    ))
 }
 
 function* login(action) {
-    try {
-        const res = yield call(api.getToken, action.email, action.password)
-        console.log(res)
+    const res = yield call(api.getToken, action.email, action.password)
+    if (res.ok) {
+        console.log(res.token)
+        localStorage.setItem('jwtToken', res.token)
         yield put(updateToken(res.token))
-    } catch(e) {
-        console.log(e)
+    } else {
+        console.log('auth failed: ' + res.message)
         yield put(authFailed())
     }
 }
